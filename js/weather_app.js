0f72b3738a7a1880b916c017acf42678143be3f5
lech-qr <lech@jet-it.pl>
2024-01-31 07:24:07 +0100

    Weather App

---
+++ js/weather_app.js
@@ -0,0 +1,64 @@
+﻿const input = document.querySelector('input')
+const button = document.querySelector('button')
+const cityName = document.querySelector('.city-name')
+const warning = document.querySelector('.warning')
+const photo = document.querySelector('.photo')
+const weather = document.querySelector('.weather')
+const temperature = document.querySelector('.temperature')
+const humidity = document.querySelector('.humidity')
+
+const API_LINK = 'https://api.openweathermap.org/data/2.5/weather?q=';
+const API_KEY = '&appid=7b1d14ef7a6cc3a8aa30474faf802bb3';
+const API_UNITS = '&units=metric'
+
+const getWeather = () => {
+    const city = input.value || 'Chicago'
+    const URL = API_LINK + city + API_KEY + API_UNITS
+
+    axios.get(URL).then(res => {
+        console.log(res.data)
+        const temp = res.data.main.temp
+        const hum = res.data.main.humidity
+        const status = Object.assign({}, ...res.data.weather)
+        console.log(status);
+
+        if (status.id >= 200 && status.id <= 232) {
+            photo.setAttribute('src', './img/thunderstorm.png')
+        } else if (status.id >= 300 && status.id <= 321) {
+            photo.setAttribute('src', './img/drizzle.png')
+        } else if (status.id >= 500 && status.id <= 531) {
+            photo.setAttribute('src', './img/rain.png')
+        } else if (status.id >= 600 && status.id <= 622) {
+            photo.setAttribute('src', './img/ice.png')
+        } else if (status.id >= 701 && status.id <= 781) {
+            photo.setAttribute('src', './img/fog.png')
+        } else if (status.id === 800) {
+            photo.setAttribute('src', './img/sun.png')
+        } else if (status.id >= 801 && status.id <= 804) {
+            photo.setAttribute('src', './img/cloud.png')
+        } else {
+            photo.setAttribute('src', './img/unknown.png')
+        }
+        
+        cityName.textContent = res.data.name
+        weather.textContent = status.main
+        temperature.textContent = Math.floor(temp) + '°C'
+        humidity.textContent = Math.floor(hum) + '%'
+        input.value = ''
+        warning.innerHTML = '&nbsp;'
+    }).catch(() => warning.textContent = 'Wpisz poprawną nazwę miasta!')
+
+
+}
+
+getWeather()
+button.addEventListener('click', getWeather)
+
+
+// Reakcja na Enter
+const enterKeyCheck = e => {
+    if (e.key === 'Enter') {
+        getWeather()
+    }
+}
+input.addEventListener('keyup', enterKeyCheck)
